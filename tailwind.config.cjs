65cc8fde1722f59cffac9c38ef960a7f402cf729
pux <nick-ng@users.noreply.github.com>
2026-06-07 20:33:37 +1200

    fix: purge is old? change back to content and specify NODE_ENV in build action

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: ["./src/**/*.html", "./src/**/*.tsx"],
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
