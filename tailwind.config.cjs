cde9ee9391a32bc480aa308130b8f07fa9a94e9d
pux <nick-ng@users.noreply.github.com>
2026-06-07 20:25:45 +1200

    feat: add purge options to tailwind.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	purge: ["./src/**/*.html", "./src/**/*.tsx"],
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
